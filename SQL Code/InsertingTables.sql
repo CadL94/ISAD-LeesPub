da034055585ec3c0da5f881603d9e949746b1d46
leecadman <leecadman@hotmail.com>
2019-11-30 00:22:22 +0000

    Sql code for inserting tables into the database

---
+++ SQL Code/InsertingTables.sql
@@ -0,0 +1,28 @@
+--Adding multiple tables to the database using INSERT INTO.
+
+INSERT INTO dbo.Tables (
+            TableName
+    
+            )
+            VALUES 
+            (
+
+            'Table one'  --Table Name
+            
+            ), 
+            (
+            
+            'Table two' --Table Name
+    
+            ),
+            (
+                
+            'Table three'  --Table Name
+            
+            ), 
+            (
+            
+            'Table four' --Table Name
+    
+            );
+          
