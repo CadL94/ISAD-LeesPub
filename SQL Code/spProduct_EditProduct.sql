7718154374e50089810fc0c40cab3f9676655571
leecadman <leecadman@hotmail.com>
2019-12-03 01:34:50 +0000

    SQL code for stored procedure - Edits already existing product on database

--- SQL Code/spProduct_EditProduct.sql
+++ SQL Code/spProduct_EditProduct.sql
@@ -2,7 +2,7 @@
 -- Cannot edit product ID as the product will have a already existing unique ID which is to stay the same.
 
 CREATE PROCEDURE dbo.spProduct_EditProduct
-@ProductID int, @CategoryID int, @ProductName NVARCHAR(50), @ProductDescription NVARCHAR(30), @QuantityPerUnit NVARCHAR(30), @Price SMALLMONEY, @UnitsInStock SMALLINT, @RestockLevel SMALLINT, @Avaliable BIT
+@ProductID int, @CategoryID int, @ProductName NVARCHAR(50), @ProductDescription NVARCHAR(30), @QuantityPerUnit NVARCHAR(30), @Price SMALLMONEY, @UnitsInStock SMALLINT, @RestockLevel SMALLINT, @Available BIT
 AS 
 BEGIN
     UPDATE  dbo.Product 
@@ -14,6 +14,6 @@ BEGIN
     Price = @Price,
     UnitsInStock = @UnitsInStock,
     RestockLevel = @RestockLevel,
-    Available = @Avaliable
+    Available = @Available
     WHERE ProductID = @ProductID
 END
