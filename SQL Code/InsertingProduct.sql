18f4e9b913973fd6c73f6f7b378928b54fdae9fd
leecadman <leecadman@hotmail.com>
2019-12-22 00:44:48 +0000

    Sql code for inserting products into the database

--- SQL Code/InsertingProduct.sql
+++ SQL Code/InsertingProduct.sql
@@ -13,55 +13,55 @@ INSERT INTO dbo.Product (
             )
             VALUES
                 (
-                    '3',   --Drinks Category 
+                    '1',   --Drinks Category 
                     'Lemon Beer', 
                     'Beer that tastes like lemon',
                     '568ml', --Quantity
                     '3.00',  --Price £
-                    '100',   --Units in stock
+                    '1000',   --Units in stock
                     '20'    --Restock level
                     
                     
                  ),
                   (
-                    '3', --Drinks Category 
+                    '1', --Drinks Category 
                     'Strawberry Beer',
                     'Beer that tastes like strawberrys',
                     '568ml', --Quantity
                     '3.00',  --Price £    
-                    '100',   --Units in stock 
+                    '1000',   --Units in stock 
                     '20'     --Restock level
                 
                     
                 ),
                 (
-                    '3', --Drinks Category 
+                    '1', --Drinks Category 
                     'Sparkling Water',
                     'Bottle of sparkling water',
                     '600ml', --Quantity
                     '1.50',  --Price £
-                    '100',   --Units in stock   
+                    '1000',   --Units in stock   
                     '20'     --Restock level
              
                 ),
                 (
-                    '4',  --Doughnuts Category 
+                    '2',  --Doughnuts Category 
                     'Pink Doughnut',
                     'A Doughnut with pink icing',
                     '1',   --Quantity
                     '1',   --Price £ 
-                    '100',  --Units in stock 
+                    '1000',  --Units in stock 
                     '20'     --Restock level
                 
                     
                 ),
 		        (
-                    '4',  --Doughnuts Category 
+                    '2',  --Doughnuts Category 
                     'Chocolate Doughnut',
                     'A Doughnut with chocolate icing',
                     '1',  --Quantity
                     '1',  --Price £ 
-                    '200',  --Units in stock 
+                    '1000',  --Units in stock 
                     '20'     --Restock level
                 
                 );
